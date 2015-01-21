rayonix/rust

rustdoc: fix fallout of merging ast::ViewItem into ast::Item.

src/librustdoc/html/format.rs
@@ -643,7 +643,7 @@ impl fmt::Show for UnsafetySpace {
     }
 }
 
-impl fmt::Show for clean::ViewPath {
+impl fmt::Show for clean::Import {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             clean::SimpleImport(ref name, ref src) => {

critiqjo/rustdoc

html/format.rs
@@ -617,7 +617,7 @@ impl fmt::String for UnsafetySpace {
     }
 }
 
-impl fmt::String for clean::ViewPath {
+impl fmt::String for clean::Import {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             clean::SimpleImport(ref name, ref src) => {